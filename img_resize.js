82bef8f64133aecae74030cc128ec6ea32e03652
Elias <ederian@noodle.org>
2013-02-13 00:59:14 -0500

    first commit

---
+++ img_resize.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+// 	$('img').imgscale({
+// 		parent : '.table1',
+// 		fade: 1000
+// 	});
+// });
+
+// $(function() {
+// 	var divElem = $(".table1");
+// 	var maxht = divElem.height() / 3;
+// 	var maxwdt = divElem.width() / 2;
+// 	var imgElem = $("img");
+// 	var ht = imgElem.height();
+// 	var wdt = imgElem.width();
+// 	if (ht < wdt) {
+// 		if (wdt > maxwdt)
+// 			$("img").css({'height' : "auto", 'width' : maxwdt});
+// 		else
+// 			$("img").css({'height' : "auto", 'width' : wdt});
+// 	}
+// 	else if (ht > wdt) {
+// 		if (ht > maxht)
+// 			$("img").css({'height' : maxht, 'width' : "auto"});
+// 		else
+// 			$("img").css({'height' : ht, 'width' : "auto"});
+// 	}
+// });
